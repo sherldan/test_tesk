e6f9bad30e5229af2828cb5dbe8334af650ff80f
sherldan <108428095+sherldan@users.noreply.github.com>
2022-08-05 17:04:01 +0300

    Update edit.blade.php

--- resources/views/client/edit.blade.php
+++ resources/views/client/edit.blade.php
@@ -10,21 +10,43 @@
                 <label for="full_name">ФИО клиента</label>
                 <input name="full_name" type="text" class="form-control" id="full_name"
                        placeholder="Введите фамилию, имя и отчество" value="{{$client->full_name}}">
+
+                @error('full_name')
+                <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
             </div>
+
             <div class="form-group">
                 <label for="phone">Телефон клиента</label>
-                <input type="text" name="phone" class="form-control" id="phone" placeholder="Введите телефон"
+                <input type="text" name="phone" class="form-control mb-3" id="phone" placeholder="Введите телефон"
                        value="{{$client->phone}}">
+
+                @error('phone')
+                <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
+
+            <div class="form-group">
+                <label for="sex">Выберите пол</label>
+                <select class="form-control" id="sex" name="sex">
+                    <option value="муж.">Мужчина</option>
+                    <option value="жен.">Женщина</option>
+                </select>
             </div>
-            <select class="form-select" name="sex">
-                <option selected>Выберите пол</option>
-                <option value="муж.">муж.</option>
-                <option value="жен.">жен.</option>
-            </select>
+
+            @error('sex')
+            <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
+
+
             <div class="form-group">
                 <label for="address">Адрес клиента</label>
                 <input name="address" type="address" class="form-control" id="address" placeholder="Введите адрес"
                        value="{{$client->address}}">
+
+                @error('address')
+                <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
+
             </div>
 
 
